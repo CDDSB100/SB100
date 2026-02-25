e27cfa0d9ff48a4c2cee3f5859a903eca570a370
melissaromao <melissadiascntt@gmail.com>
2026-02-25 02:16:59 +0000

    Sincronização: Estrutura organizada das squads

---
+++ squad3/testeQdrant.py
@@ -0,0 +1,11 @@
+from qdrant_client import QdrantClient
+url_qdrant = "http://10.147.20.52:6333"
+api_key_qdrant = "fwaoNYhMTH3vf2QfzrxajQ=="
+try:
+    print(f"Tentando conectar ao Qdrant em {url_qdrant}...")
+    client = QdrantClient(url=url_qdrant, api_key=api_key_qdrant)
+    collections = client.get_collections()
+    print("Conexão bem-sucedida!")
+    print(f"Coleções encontradas: {collections}")
+except Exception as e:
+    print(f"Falha na conexão: {e}")
