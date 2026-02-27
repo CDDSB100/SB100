610f9370a3e085851ef500f6418ca9a13995af92
luisciaramicoli <luisgustavobonfim996@gmail.com>
2026-02-27 17:23:16 -0300

    nova configuração

--- squad1/ciento/api-cientometria/server.js
+++ squad1/ciento/api-cientometria/server.js
@@ -104,8 +104,8 @@ app.use((req, res, next) => {
   next();
 });
 
-// Se existir uma build do front-end, servir os arquivos estáticos
-const frontendBuildPath = path.join(__dirname, '../busca-cientometrica/dist');
+// Se existir uma build do front-end (agora em frontend/dist), servir os arquivos estáticos
+const frontendBuildPath = path.join(__dirname, '../frontend/dist');
 if (fsSync.existsSync(frontendBuildPath)) {
   console.log(`> Servindo frontend estático de ${frontendBuildPath}`);
   // servir sem cache para evitar antigos conteúdos em browsers
