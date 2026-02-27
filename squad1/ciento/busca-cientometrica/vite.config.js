910873480d6bba638805cf656541f51c3bc63f57
luisciaramicoli <luisgustavobonfim996@gmail.com>
2026-02-27 17:06:07 -0300

    remoção de arquivos inuteis e configuração

--- squad1/ciento/busca-cientometrica/vite.config.js
+++ squad1/ciento/busca-cientometrica/vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, 'node_modules/react'),
+      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+      '@emotion/react': path.resolve(__dirname, 'node_modules/@emotion/react')
+    }
+  },
   server: {
     allowedHosts: ['sb100cientometria.optin.com.br', 'localhost', '127.0.0.1', '172.28.181.92', '0.0.0.0'],
     middlewareMode: false,
