5df5865e8033af2c7593d832c58167ff97d08d35
luisciaramicoli <luisgustavobonfim996@gmail.com>
2026-02-27 12:36:02 -0300

    Update vite.config.js

--- squad1/ciento/busca-cientometrica/vite.config.js
+++ squad1/ciento/busca-cientometrica/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['sb100cientometria.optin.com.br'],
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:5001',
