0a15935235a99980f89d8962e6544fe1b71d23da
luisciaramicoli <luisgustavobonfim996@gmail.com>
2026-03-10 13:32:46 -0300

    Update vite.config.js

--- squad1/frontend/vite.config.js
+++ squad1/frontend/vite.config.js
@@ -1,18 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      'react': path.resolve(__dirname, './node_modules/react'),
-      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
-      '@emotion/react': path.resolve(__dirname, './node_modules/@emotion/react'),
-      '@emotion/styled': path.resolve(__dirname, './node_modules/@emotion/styled'),
-      '@mui/material': path.resolve(__dirname, './node_modules/@mui/material'),
-    },
+    // Apenas deduplicação, sem aliases fixos que quebram sub-caminhos
     dedupe: ['react', 'react-dom', '@emotion/react', '@emotion/styled', '@mui/material']
   },
   server: {
